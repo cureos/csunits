6026ec1097479a4f9f9be59033b6e97b3aa93dbb
cureos <anders@cureos.com>
2011-08-03 17:13:18 +0200

    Added convenience methods for creating IEnumerable StandardMeasure collections

---
+++ Cureos.Measures/Linq/StandardMeasureEnumerable.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) 2011 Anders Gustafsson, Cureos AB.
+// All rights reserved. This software and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cureos.Measures.Linq
+{
+    public static class StandardMeasureEnumerable
+    {
+        /// <summary>
+        /// Cast collection of amounts into corresponding StandardMeasure enumerable
+        /// </summary>
+        /// <typeparam name="Q">Quantity of measurements</typeparam>
+        /// <param name="iAmounts">Collection of amounts to be cast into a collection of standard measures</param>
+        /// <returns>Collection of standard measures</returns>
+        public static IEnumerable<StandardMeasure<Q>> ToStandardMeasures<Q>(this IEnumerable<double> iAmounts) where Q : struct, IQuantity<Q>
+        {
+            return iAmounts.Select(val => new StandardMeasure<Q>(val));
+        }
+
+        /// <summary>
+        /// Cast collection of amounts into corresponding StandardMeasure enumerable
+        /// </summary>
+        /// <typeparam name="Q">Quantity of measurements</typeparam>
+        /// <param name="iAmounts">Collection of amounts to be cast into a collection of standard measures</param>
+        /// <param name="iUnit">Unit in which the amounts are specified</param>
+        /// <returns>Collection of standard measures, converted from specified unit into quantity's standard unit</returns>
+        public static IEnumerable<StandardMeasure<Q>> ToStandardMeasures<Q>(this IEnumerable<double> iAmounts, IUnit<Q> iUnit) where Q : struct, IQuantity<Q>
+        {
+            return iAmounts.Select(val => new StandardMeasure<Q>(val, iUnit));
+        }
+
+        /// <summary>
+        /// Cast collection of amounts into corresponding StandardMeasure enumerable
+        /// </summary>
+        /// <typeparam name="Q">Quantity of measurements</typeparam>
+        /// <param name="iAmounts">Collection of amounts to be cast into a collection of standard measures</param>
+        /// <returns>Collection of standard measures</returns>
+        public static IEnumerable<StandardMeasure<Q>> ToStandardMeasures<Q>(this IEnumerable<float> iAmounts) where Q : struct, IQuantity<Q>
+        {
+            return iAmounts.Select(val => new StandardMeasure<Q>(val));
+        }
+
+        /// <summary>
+        /// Cast collection of amounts into corresponding StandardMeasure enumerable
+        /// </summary>
+        /// <typeparam name="Q">Quantity of measurements</typeparam>
+        /// <param name="iAmounts">Collection of amounts to be cast into a collection of standard measures</param>
+        /// <param name="iUnit">Unit in which the amounts are specified</param>
+        /// <returns>Collection of standard measures, converted from specified unit into quantity's standard unit</returns>
+        public static IEnumerable<StandardMeasure<Q>> ToStandardMeasures<Q>(this IEnumerable<float> iAmounts, IUnit<Q> iUnit) where Q : struct, IQuantity<Q>
+        {
+            return iAmounts.Select(val => new StandardMeasure<Q>(val, iUnit));
+        }
+
+        /// <summary>
+        /// Cast collection of amounts into corresponding StandardMeasure enumerable
+        /// </summary>
+        /// <typeparam name="Q">Quantity of measurements</typeparam>
+        /// <param name="iAmounts">Collection of amounts to be cast into a collection of standard measures</param>
+        /// <returns>Collection of standard measures</returns>
+        public static IEnumerable<StandardMeasure<Q>> ToStandardMeasures<Q>(this IEnumerable<decimal> iAmounts) where Q : struct, IQuantity<Q>
+        {
+            return iAmounts.Select(val => new StandardMeasure<Q>(val));
+        }
+
+        /// <summary>
+        /// Cast collection of amounts into corresponding StandardMeasure enumerable
+        /// </summary>
+        /// <typeparam name="Q">Quantity of measurements</typeparam>
+        /// <param name="iAmounts">Collection of amounts to be cast into a collection of standard measures</param>
+        /// <param name="iUnit">Unit in which the amounts are specified</param>
+        /// <returns>Collection of standard measures, converted from specified unit into quantity's standard unit</returns>
+        public static IEnumerable<StandardMeasure<Q>> ToStandardMeasures<Q>(this IEnumerable<decimal> iAmounts, IUnit<Q> iUnit) where Q : struct, IQuantity<Q>
+        {
+            return iAmounts.Select(val => new StandardMeasure<Q>(val, iUnit));
+        }
+    }
+}
---
+++ Tests.Cureos.Measures/Linq/StandardMeasureEnumerableTests.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) 2011 Anders Gustafsson, Cureos AB.
+// All rights reserved. This software and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+
+using System;
+using System.Linq;
+using Cureos.Measures;
+using Cureos.Measures.Linq;
+using Cureos.Measures.Quantities;
+using NUnit.Framework;
+
+namespace Tests.Cureos.Measures.Linq
+{
+    [TestFixture]
+    public class StandardMeasureEnumerableTests
+    {
+        #region Unit tests
+
+        [Test]
+        public void ToStandardMeasures_DoubleNoUnitConversion_ReturningNonConvertedStandardMeasures()
+        {
+            var measures = new[] { 1.0, 2.0, 3.0, -2.0 }.ToStandardMeasures<Length>();
+            var expected = new StandardMeasure<Length>(3.0);
+            var actual = measures.ElementAt(2);
+            MeasureAssert.MeasuresAreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ToStandardMeasures_DecimalWithUnitConversion_ReturningConvertedStandardMeasures()
+        {
+            var measures = new[] { 1.0m, 2.0m, 3.0m, -2.0m }.ToStandardMeasures(ElectricCurrent.MilliAmpere);
+            var expected = new StandardMeasure<ElectricCurrent>(0.002m);
+            var actual = measures.ElementAt(1);
+            MeasureAssert.MeasuresAreEqual(expected, actual);
+        }
+
+        #endregion
+    }
+}
