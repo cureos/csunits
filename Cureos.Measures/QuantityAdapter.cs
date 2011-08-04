eb04e69896efab5f90e62510c0745d0aac1283b1
cureos <anders@cureos.com>
2011-08-04 10:36:31 +0200

    Added base comment to ToString() method

--- Cureos.Measures/QuantityAdapter.cs
+++ Cureos.Measures/QuantityAdapter.cs
@@ -9,47 +9,53 @@ using System.Collections.Generic;
 
 namespace Cureos.Measures
 {
-    /// <summary>
-    /// Convenience class for accessing a quantity and its associated units
-    /// </summary>
-    public class QuantityAdapter
-    {
-        #region CONSTRUCTORS
+	/// <summary>
+	/// Convenience class for accessing a quantity and its associated units
+	/// </summary>
+	public class QuantityAdapter
+	{
+		#region CONSTRUCTORS
 
-        /// <summary>
-        /// Initializes an instance of a wrapper object containing a quantity and its associated units
-        /// </summary>
-        /// <param name="iQuantity">Quantity</param>
-        /// <param name="iUnits">Units associated with the quantity</param>
-        public QuantityAdapter(IQuantity iQuantity, IEnumerable<IUnit> iUnits)
-        {
-            Quantity = iQuantity;
-            Units = iUnits;
-        }
+		/// <summary>
+		/// Initializes an instance of a wrapper object containing a quantity and its associated units
+		/// </summary>
+		/// <param name="iQuantity">Quantity</param>
+		/// <param name="iUnits">Units associated with the quantity</param>
+		public QuantityAdapter(IQuantity iQuantity, IEnumerable<IUnit> iUnits)
+		{
+			Quantity = iQuantity;
+			Units = iUnits;
+		}
 
-        #endregion
+		#endregion
 
-        #region AUTO-IMPLEMENTED PROPERTIES
+		#region AUTO-IMPLEMENTED PROPERTIES
 
-        /// <summary>
-        /// Gets the contained quantity
-        /// </summary>
-        public IQuantity Quantity { get; private set; }
+		/// <summary>
+		/// Gets the contained quantity
+		/// </summary>
+		public IQuantity Quantity { get; private set; }
 
-        /// <summary>
-        /// Gets the units associated with the quantity
-        /// </summary>
-        public IEnumerable<IUnit> Units { get; private set; }
+		/// <summary>
+		/// Gets the units associated with the quantity
+		/// </summary>
+		public IEnumerable<IUnit> Units { get; private set; }
 
-        #endregion
+		#endregion
 		
 		#region OVERRIDDEN METHODS
-		
+
+		/// <summary>
+		/// Returns a <see cref="T:System.String"/> that represents the current <see cref="T:System.Object"/>.
+		/// </summary>
+		/// <returns>
+		/// A <see cref="T:System.String"/> that represents the current <see cref="T:System.Object"/>.
+		/// </returns>
 		public override string ToString ()
 		{
 			return Quantity.ToString();
 		}
 		
 		#endregion
-    }
+	}
 }
